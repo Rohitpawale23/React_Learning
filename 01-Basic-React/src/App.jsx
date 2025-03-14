82bfa6683735e83d7299cdc1cfc8280a23946be5
Rohitpawale23 <pawalerohit863@gmail.com>
2025-03-14 18:07:20 +0530

    01-React-Basics

---
+++ 01-Basic-React/src/App.jsx
@@ -0,0 +1,23 @@
+
+
+
+function App() {
+
+  return (
+<div>
+  <h1>React with Vite | Rohit </h1>
+  <h2> Today's learning</h2>
+  <p>In this tutorial by Chai aur Code YouTube channel we understood how to create an React project different types of creating react project and how to read 
+    react project read from package.json file</p>
+    <p>Two types for creating react project are  : </p>
+    <ol>
+      <li>Using npx create reactapp : It takes long time for setup the environment</li>
+      <li>Using npm create vite@latest : It takes less time widely used</li>
+    </ol>
+</div>
+
+
+  )
+}
+
+export default App
